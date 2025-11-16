0f9e6a523349add0583d59163d7b4fd7b32765fc
Carllos Danyel <128504469+CarllosDanyel@users.noreply.github.com>
2025-11-16 18:46:40 -0300

    entrega 3

---
+++ src/main/java/com/meuprojeto/App.java
@@ -0,0 +1,10 @@
+package com.meuprojeto;
+
+public class App {
+    public static void main(String[] args) {
+    Utils.mensagem();
+    System.out.println("ja");
+    System.out.println("ja");
+    int a = 10;
+}
+}
